fa7f0226d8bdb171467cf038b7ff1f6485f1a4b8
sivabandaru31 <siva.bandaru31@gmail.com>
2026-09-26 20:30:32 +0530

    Added named and default exports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import './App.css';
 import Welcome from './components/Welcome';
 import Greeting from './components/Greeting';
-
+import {FirstComponents as FC, SecondComponents as SC} from './components/MyComponents';
+import MC from './components/MyComponents';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,12 @@ function App() {
       <Welcome name="raja"/> */}
       <Greeting/>
       <Greeting name="google"/>
+      {/* <FirstComponents/> */}
+      <FC/>
+      {/* <SecondComponents/> */}
+      <SC/>
+      {/* <MyComponent/> */}
+      <MC/>
     </div>
   );
 }
---
+++ src/components/MyComponents.jsx
@@ -0,0 +1,17 @@
+// function FirstComponents(){
+//     return <h1>First Component</h1>
+// }
+// export default FirstComponents//default export 
+
+export function FirstComponents(){//named export
+    return <h1>First Component</h1>
+}
+
+export function SecondComponents(){
+    return <h1>Second Component</h1>
+}
+
+export default function MyComponent(){
+    return <h1>My Component</h1>
+
+}
